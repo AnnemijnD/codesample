ceea5ac0fb1abfdd22a3cbb6862ac63e1059c502
AnnemijnD <annemijn1@live.nl>
2023-11-03 14:27:11 +0100

    edited scatterplot

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,3 +1,5 @@
+// To see page: https://annemijnd.github.io/codesample/html/scat_freq_acc.html
+
 // Define data path and import data
 var datapath = "../";
 try {
@@ -24,9 +26,9 @@ d3.csv(datapath + 'results/features/IG100.csv', function loadCallback(error, dat
 // Common pattern for defining vis size and margins
 var makeVis2 = function(data) {
 
-    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+    var margin = { top: 100, right: 20, bottom: 100, left: 100 },
         width  = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 600 - margin.top - margin.bottom;
 
     // Add the visualization svg canvas to the vis-container <div>
     var canvas = d3.select("#vis-container").append("svg")
@@ -39,14 +41,15 @@ var makeVis2 = function(data) {
     var colorScale = d3.scale.category10();
 
     var xScale = d3.scale.linear()
-        .domain([ 0,
-                  d3.max(data, function(d) { return d.freqs; }) + 1 ])
-        .range([0, width]);
+        .domain([ 0, d3.max(data, function(d) { return d.freqs; }) + 1 ])
+        .range([0, width])
+        .nice();
+
 
     var yScale = d3.scale.linear()
-        .domain([ d3.min(data, function(d) { return d.accs; }) - 1,
-                  d3.max(data, function(d) { return d.accs; }) + 1 ])
-        .range([height, 0]); // flip order because y-axis origin is upper LEFT
+        .domain([ 0,1 ])
+        .range([height, 0])
+        .nice();
 
     // Define our axes
     var xAxis = d3.svg.axis()
@@ -57,15 +60,16 @@ var makeVis2 = function(data) {
         .scale(yScale)
         .orient('left');
 
+
     // Add x-axis to the canvas
     canvas.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")") // move axis to the bottom of the canvas
+        .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
       .append("text")
         .attr("class", "label")
-        .attr("x", width)
-        .attr("y", -6)
+        .attr("x", width/2 + 30)
+        .attr("y", 50)
         .style("text-anchor", "end")
         .text("Frequency");
 
@@ -76,9 +80,28 @@ var makeVis2 = function(data) {
         .append("text")
         .attr("class", "label")
         .attr("transform", "rotate(-90)")
-        .attr("y", 15)
+        .attr("y",  - margin.left/2)
+        .attr("x", (-height+margin.top)/2)
         .style("text-anchor", "end")
         .text("Mean Accuracy");
+
+    // Add title
+    canvas.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "20px")
+        .style("text-decoration", "underline")
+        .text("Frequency and accuracy of selected features");
+
+    // Add subtitle
+    canvas.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2) + 20)
+        .attr("text-anchor", "middle")
+        .style("font-size", "10px")
+        .text("A plot that shows how often a feature was selected and the average accuracy of the models it was used for. \n Hover over the dots to see the feature number and frequency. ");
+
 
     // Add the tooltip container to the vis container
     // It's invisible and its position/contents are defined during mouseover
@@ -88,7 +111,6 @@ var makeVis2 = function(data) {
 
     // Tooltip mouseover event handler
     var tipMouseover = function(d) {
-        // var color = colorScale(d.manufacturer);
         var html  = "Frequency: " +d.freqs + "</br>" + "Feature: "+ d.features;
 
         tooltip.html(html)
@@ -103,19 +125,27 @@ var makeVis2 = function(data) {
     // Tooltip mouseout event handler
     var tipMouseout = function(d) {
         tooltip.transition()
-            .duration(300) // ms
-            .style("opacity", 0);
+                .duration(300) // ms
+                .style("opacity", 0);
     };
 
     // Add data points
     canvas.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3)
-      .attr("cx", function(d) { return xScale( d.freqs ); })     // x position
-      .attr("cy", function(d) { return yScale( d.accs ); })  // y position
-      .style("fill", function(d) { return colorScale(d.freqs); })
-      .on("mouseover", tipMouseover)
-      .on("mouseout", tipMouseout);
+          .data(data)
+          .enter()
+          .append("circle")
+          .attr("class", "dot")
+          .attr("r", 3)
+          // .transition()
+          // .delay(function(d,i){return(i*3)})
+          // .duration(2000)
+          .attr("cx", function(d) { return xScale( d.freqs ); })     // x position
+          .attr("cy", function(d) { return yScale( d.accs ); })  // y position
+          .style("fill", function(d) { return colorScale(d.freqs); })
+
+  canvas.selectAll(".dot")
+        .on("mouseover", tipMouseover)
+        .on("mouseout", tipMouseout)
+
+
 };
